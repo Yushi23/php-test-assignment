effa0b708be8265e6ca1833e94da82f0e86d6a06
Yushi23 <ayushiev2014@mail.ru>
2024-04-29 20:22:29 +0800

    Add contollers

---
+++ app/controllers/LoadController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Comment;
+use App\Models\DataFromUrl;
+use App\Models\Post;
+use Database\DB;
+
+class LoadController
+{
+    public function run()
+    {
+        $dataUrl = new DataFromUrl();
+        $db = DB::getInstance(parse_ini_file('../.env'));
+
+        $post = new Post($db);
+        $post->insertToTable($dataUrl->getData('https://jsonplaceholder.typicode.com/posts'));
+
+        $comment = new Comment($db);
+        $comment->insertToTable($dataUrl->getData('https://jsonplaceholder.typicode.com/comments'));
+
+        print_r("Загружено {$post->getRecordsAfterChange()} записей и {$comment->getRecordsAfterChange()} комментариев");
+    }
+}
